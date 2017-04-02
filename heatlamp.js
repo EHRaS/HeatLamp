f95c6c7aa56b383122b46a299b5bee9829264411
Jack Kingsman <jack.kingsman@gmail.com>
2017-04-02 10:30:57 -0700

    Fix placement of expiration calculation and reduce length

--- heatlamp.js
+++ heatlamp.js
@@ -1,7 +1,6 @@
 module.exports = function(expirationLimits){
-    const hoursExpiration = Math.floor(Math.random() * (expirationLimits.max - expirationLimits.min)) + expirationLimits.min;
-    const secondsExpiration = hoursExpiration * 3600;
     return function (req, res, next) {
+        const secondsExpiration = Math.floor(Math.random() * ((expirationLimits.max * 3600) - (expirationLimits.min * 3600))) + (expirationLimits.min * 3600);
         res.header('Cache-Control', 'max-age=' + secondsExpiration + ', must-revalidate');
         next();
     };
